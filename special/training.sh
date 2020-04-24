0d12ad29d4846eacfb07f233bae0b1e6e3469161
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-24 15:06:16 +0200

    Add special schedule

---
+++ special/training.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+espeak "Squat"; sleep 30
+espeak "Lunge"; sleep 30
+espeak "Break"; sleep 45
+
+espeak "Wide push up"; sleep 30
+espeak "Torso elevated push up"; sleep 30
+espeak "Break"; sleep 45
+
+espeak "Single leg bridge"; sleep 30
+espeak "Leg bridge"; sleep 30
+espeak "Break"; sleep 45
+
+espeak "Front knee raise"; sleep 30
+espeak "Heel raise"; sleep 30
+espeak "Break"; sleep 45
+
+espeak "Kneeling arm and leg"; sleep 30
+espeak "Lying arm raise"; sleep 30
+espeak "Break"; sleep 45
+
+espeak "Crunch"; sleep 30
+espeak "Lying leg extention"; sleep 30
+espeak "Break"; sleep 45
+
+espeak "Burpee"; sleep 30
+espeak "Mountain climber"; sleep 30
+espeak "Break"; sleep 45
+
+
+
