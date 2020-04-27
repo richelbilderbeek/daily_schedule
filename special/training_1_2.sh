30e94b1b20f8beebc83da1ea01f4bb6e9d901541
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-27 20:22:25 +0200

    EOD

---
+++ special/training_1_2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+espeak "Round 1 of 7. Stand straight"; sleep 5
+
+espeak "Squat heel raise"; sleep 30
+espeak "Wall sit"; sleep 30
+espeak "Break"; sleep 40
+
+espeak "Round 2 of 7. Lie on front"; sleep 5
+
+espeak "Diagonal push up, knees"; sleep 30
+espeak "Front plank"; sleep 30
+espeak "Break"; sleep 40
+
+espeak "Round 3 of 7. Lie on back"; sleep 5
+
+espeak "High leg bridge"; sleep 30
+espeak "Straight arm leg bridge"; sleep 30
+espeak "Break"; sleep 40
+
+espeak "Round 4 of 7. Lie on back"; sleep 5
+
+espeak "Front knee raise front kick"; sleep 30
+espeak "Box heel raise"; sleep 30
+espeak "Break"; sleep 40
+
+espeak "Round 5 of 7. Stand straight"; sleep 5
+
+espeak "Standing leg raise"; sleep 30
+espeak "Lying leg raise"; sleep 30
+espeak "Break"; sleep 40
+
+espeak "Round 6 of 7. Lie on back"; sleep 5
+
+espeak "Elevated crunch"; sleep 30
+espeak "Straight leg crunch"; sleep 30
+espeak "Break"; sleep 40
+
+espeak "Last round. Stand straight"; sleep 5
+
+espeak "Burpee"; sleep 30
+espeak "Push up"; sleep 30
+espeak "Done"
+
+
+
