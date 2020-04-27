7493f2d89ecbc5a39a148554f6ac8fe6aadc88f3
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-27 16:41:22 +0200

    Count rounds

--- special/training_1_1.sh
+++ special/training_1_1.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
+espeak "Round 1 of 7. Stand straight"; sleep 5
+
 espeak "Squat"; sleep 30
 espeak "Lunge"; sleep 30
-espeak "Break"; sleep 45
+espeak "Break"; sleep 40
+
+espeak "Round 2 of 7. Lie on front"; sleep 5
 
 espeak "Wide push up"; sleep 30
 espeak "Torso elevated push up"; sleep 30
-espeak "Break"; sleep 45
+espeak "Break"; sleep 40
+
+espeak "Round 3 of 7. Lie on back"; sleep 5
 
 espeak "Single leg bridge"; sleep 30
 espeak "Leg bridge"; sleep 30
-espeak "Break"; sleep 45
+espeak "Break"; sleep 40
+
+espeak "Round 4 of 7. Lie on back"; sleep 5
 
 espeak "Front knee raise"; sleep 30
 espeak "Heel raise"; sleep 30
-espeak "Break"; sleep 45
+espeak "Break"; sleep 40
+
+espeak "Round 5 of 7. Stand straight"; sleep 5
 
 espeak "Kneeling arm and leg"; sleep 30
 espeak "Lying arm raise"; sleep 30
-espeak "Break"; sleep 45
+espeak "Break"; sleep 40
+
+espeak "Round 6 of 7. Lie on back"; sleep 5
 
 espeak "Crunch"; sleep 30
 espeak "Lying leg extention"; sleep 30
-espeak "Break"; sleep 45
+espeak "Break"; sleep 40
+
+espeak "Last round. Stand straight"; sleep 5
 
 espeak "Burpee"; sleep 30
 espeak "Mountain climber"; sleep 30
-espeak "Break"; sleep 45
+espeak "Done"
 
 
 
