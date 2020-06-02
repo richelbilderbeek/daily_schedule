b21a395114ae14b77d27c98401a3757eee067c01
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-02 06:28:13 +0200

    EOD

---
+++ special/minecraft_quest.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "prep meeting mods"' | at 8:55
+echo 'espeak -s 120 -p 10 "meeting mods"' | at 9:00
+echo 'espeak -s 120 -p 10 "Discord"' | at 9:45
+echo 'espeak -s 120 -p 10 "Start"' | at 10:00
+echo 'espeak -s 120 -p 10 "Meeting"' | at 10:15
+
