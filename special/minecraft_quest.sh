0c9b5ff2dccc91bcc602a477fb08b054fa0d4dfe
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-08 14:46:44 +0200

    EOD

--- special/minecraft_quest.sh
+++ special/minecraft_quest.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-echo 'espeak -s 120 -p 10 "prep meeting mods"' | at 8:55
-echo 'espeak -s 120 -p 10 "meeting mods"' | at 9:00
-echo 'espeak -s 120 -p 10 "Discord"' | at 9:45
-echo 'espeak -s 120 -p 10 "Start"' | at 10:00
-echo 'espeak -s 120 -p 10 "Meeting"' | at 10:15
+echo 'espeak -s 120 -p 10 "prep meeting mods"' | at 14:45
+echo 'espeak -s 120 -p 10 "start"' | at 15:00
+echo 'espeak -s 120 -p 10 "break"' | at 15:45
+echo 'espeak -s 120 -p 10 "Start"' | at 16:00
+echo 'espeak -s 120 -p 10 "end"' | at 17:00
 
