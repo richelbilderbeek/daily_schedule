f7220db5ae043aa02543bbd4d7be78d3737e3a1a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-15 06:21:10 +0200

    EOD

--- special/minecraft_quest.sh
+++ special/minecraft_quest.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-echo 'espeak -s 120 -p 10 "prep meeting mods"' | at 14:45
-echo 'espeak -s 120 -p 10 "start"' | at 15:00
-echo 'espeak -s 120 -p 10 "break"' | at 15:45
-echo 'espeak -s 120 -p 10 "Start"' | at 16:00
-echo 'espeak -s 120 -p 10 "end"' | at 17:00
+echo 'espeak -s 120 -p 10 "door open"' | at 15:00
+echo 'espeak -s 120 -p 10 "server open"' | at 15:05
+echo 'espeak -s 120 -p 10 "end of warming up"' | at 15:35
+echo 'espeak -s 120 -p 10 "setup real game"' | at 15:40
+echo 'espeak -s 120 -p 10 "determine winner"' | at 16:50
 
