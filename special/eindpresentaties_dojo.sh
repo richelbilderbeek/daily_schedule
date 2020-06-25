1bbe47887ef7a7cfc8e1f0d91e17231a48eea008
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-25 17:41:19 +0200

    EOD

---
+++ special/eindpresentaties_dojo.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "be there"' | at 17:45
+echo 'espeak -s 120 -p 10 "kids are welcome"' | at 18:15
+echo 'espeak -s 120 -p 10 "parents are welcome"' | at 18:30
+echo 'espeak -s 120 -p 10 "start presentation"' | at 18:45
+echo 'espeak -s 120 -p 10 "start evaluation"' | at 19:15
+
