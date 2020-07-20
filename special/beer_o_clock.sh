1af58e1b00fd9e1f9a62daf839993d5577912760
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-20 07:33:07 +0200

    EOD

---
+++ special/beer_o_clock.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "beer o clock. beer o clock. beer o clock. beer o clock. beer o clock. beer o clock. beer o clock. beer o clock. beer o clock. beer o clock. "' | at 17:00
