9ae95f72293512952ec64615130d88107a25b578
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-02 11:48:22 +0200

    EOD

--- special/informalr.sh
+++ special/informalr.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+echo 'espeak -s 120 -p 10 "go to entrance"' | at 17:55
 echo 'espeak -s 120 -p 10 "welcome to informalr"' | at 18:00
 echo 'espeak -s 120 -p 10 "start to work"' | at 18:15
 echo 'espeak -s 120 -p 10 "time to have a break"' | at 19:00
