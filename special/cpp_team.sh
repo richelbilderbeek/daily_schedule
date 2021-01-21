a365b17a6cfb77cca5e142cbd6ca1503a1e71d88
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-21 09:52:17 +0100

    EOD

---
+++ special/cpp_team.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "welcome to informalr"' | at 17:00
+echo 'espeak -s 120 -p 10 "start to work"' | at 17:15
+echo 'espeak -s 120 -p 10 "time to have a break"' | at 18:00
+echo 'espeak -s 120 -p 10 "back to work"' | at 18:15
+echo 'espeak -s 120 -p 10 "enjoy the rest of your day"' | at 19:00
