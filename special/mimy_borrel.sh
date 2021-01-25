10580f55a89429b8db257ae63465a255269b91f6
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-25 11:50:58 +0100

    Add GitHub Actions logo

--- special/mimy_borrel.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-echo 'espeak -s 120 -p 10 "prepare"' | at 15:45
-echo 'espeak -s 120 -p 10 "start borrel"' | at 16:00
-echo 'espeak -s 120 -p 10 "time to leave"' | at 16:55
---
+++ special/molimm_borrel.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "prepare for borrel"' | at 15:45
+echo 'espeak -s 120 -p 10 "start borrel"' | at 16:00
+echo 'espeak -s 120 -p 10 "time to leave"' | at 19:55
