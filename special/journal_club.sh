bdd3bed58c5f694974dc22f3319c67774c73dc86
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-17 15:17:15 +0200

    Cannot do 'ggplot2::ggplot(...) + ggplot2::ggsave(...)', do 'ggplot2::ggplot(...); ggplot2::ggsave(...)' instead, see https://github.com/tidyverse/ggplot2/issues/4517

---
+++ special/journal_club.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "welcome"' | at 9:00
+echo 'espeak -s 120 -p 10 "volunteers"' | at 9:58
+echo 'espeak -s 120 -p 10 "bye"' | at 9:59
---
+++ special/pair_programming_8_mins.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "start to work"' | at now
+echo 'espeak -s 120 -p 10 "switch"' | at now + 8 min
+echo 'espeak -s 120 -p 10 "switch"' | at now + 16 min
+echo 'espeak -s 120 -p 10 "switch"' | at now + 24 min
+echo 'espeak -s 120 -p 10 "switch"' | at now + 32 min
+echo 'espeak -s 120 -p 10 "switch"' | at now + 40 min
+echo 'espeak -s 120 -p 10 "switch"' | at now + 48 min
+
