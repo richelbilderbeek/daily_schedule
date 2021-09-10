9b3769d56a51668694cfd7a529970e210e307998
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-10 12:28:17 +0200

    Use a semicolon instead of a plus before ggplot2::ggsave, as the plus now gives a rightful error

--- special/journal_club.sh
+++ special/journal_club.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 echo 'espeak -s 120 -p 10 "welcome"' | at 9:00
-echo 'espeak -s 120 -p 10 "volunteers"' | at 9:58
+echo 'espeak -s 120 -p 10 "no more questions"' | at 9:55
+echo 'espeak -s 120 -p 10 "next journal clubs"' | at 9:58
 echo 'espeak -s 120 -p 10 "bye"' | at 9:59
