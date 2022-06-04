88e73801d21cfc9a8176b342df385a09b097ae10
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-04 12:25:59 +0200

    EOD

---
+++ special/journal_club_20220602.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+echo 'espeak -s 120 -p 10 "welcome"' | at 9:00
+echo 'espeak -s 120 -p 10 "no more questions"' | at 9:40
+echo 'espeak -s 120 -p 10 "next weeks speakers"' | at 9:43
+echo 'espeak -s 120 -p 10 "start evaluation"' | at 9:45
+echo 'espeak -s 120 -p 10 "close meeting"' | at 9:59
