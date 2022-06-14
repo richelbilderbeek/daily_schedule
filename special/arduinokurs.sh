d7b1409ad609a498d1bc550ec04e55d50e7b6c6e
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-14 15:01:02 +0200

    EOD

---
+++ special/arduinokurs.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Dör öppet"' | at 12:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Klockan starta"' | at 12:45
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 13:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 13:45
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Klockan slut"' | at 14:30
+
+
