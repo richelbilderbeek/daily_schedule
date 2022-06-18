730995643643fbd93340c854eee8ba15dfb50f11
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-18 08:58:59 +0200

    EOD

--- special/journal_club.sh
+++ special/journal_club.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+echo 'espeak -s 120 -p 10 "go online"' | at 8:50
 echo 'espeak -s 120 -p 10 "welcome"' | at 9:00
 echo 'espeak -s 120 -p 10 "no more questions"' | at 9:55
 echo 'espeak -s 120 -p 10 "next journal clubs"' | at 9:58
