30f0abd6367148f53081ea042627642f729809db
richelbilderbeek <richel@richelbilderbeek.nl>
2022-11-25 17:26:25 +0100

    EOD

---
+++ special/stenhagen.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Dör öppnas"' | at 16:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Klockan starta"' | at 16:45
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 17:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 17:45
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tiden är slut"' | at 18:30
+
+
