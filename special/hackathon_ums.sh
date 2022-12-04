a18fca2ae3df984b103d69b65d9fc17cc31955e8
richelbilderbeek <richel@richelbilderbeek.nl>
2022-12-04 18:02:46 +0100

    EOD

--- special/hackathon_ums.sh
+++ special/hackathon_ums.sh
@@ -5,12 +5,12 @@ echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en
 echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 13:15
 echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 14:00
 echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 14:15
-echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 15:00
-echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 15:15
-echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 16:00
-echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 16:15
-echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 17:00
-echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 17:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid foer pizza"' | at 15:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 15:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 16:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 16:30
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 17:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 17:30
 echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Klockan slut"' | at 18:00
 
 
