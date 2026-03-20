709a8f9e42df7027e8200ae6bd5e9440c91cebc9
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-03-20 10:21:43 +0100

    EOD

---
+++ special/prao.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 10:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 10:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 11:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 11:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 12:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 13:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 14:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 14:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tid att ta en paus"' | at 15:00
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tillbaks till arbete"' | at 15:15
+echo 'espeak -v swedish-mbrola-1 -v swedish-mbrola-1 -s 120 -p 10 "Tiden är slut"' | at 16:00
+
+
